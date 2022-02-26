0c824a1397b32b1b4753a17b4a8c01a672197cfd
KseniaShiryaeva <100152965+KseniaShiryaeva@users.noreply.github.com>
2022-02-26 18:33:41 +0300

    add speed

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,45 @@
-
-
 public class Main {
-    // статические переменные (я не до конца поняла как это сделать)
 
-    static class Collors {
-        static int tsGreenLight;
-        private int speedOfPlayer1;
-        private int speedOfPlayer2;
-        private int speedOfPlayer3;
-
-    }
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
         // true-зелёный false-красный
-        boolean itsGreenLight = false;
+        boolean itsGreenLight = true;
 
         //игроки
         int speedOfPlayer1 = 0;
-        int speedOfPlayer2 = 2;
+        int speedOfPlayer2 = 6;
         int speedOfPlayer3 = 7;
 
         // кол-во выбывших
         int count = 0;
+        int countSpeed = speedOfPlayer1;
+        int countSpeed2 = speedOfPlayer2;
+        int countSpeed3 = speedOfPlayer3;
 
         //Создаем массив игроков (собираем их в кучку чтобы можно было их перечислить)
         int players[] = {speedOfPlayer1, speedOfPlayer2, speedOfPlayer3};
+        int speed [] = {speedOfPlayer1, speedOfPlayer2, speedOfPlayer3};
 
         // если горит красный
         if (itsGreenLight == false) {
-            // перебираем игроков
+            // перебираем игроков;
             for (int i = 0; i < 3; i++) {
                 //если скорость игрока не 0 то count увеличивается на 1
                 if (players[i] != 0) count++;
             }
             System.out.println("Горит красный, выбывает " + count + " игроков");
+            while (speedOfPlayer1 < 0)
+                for (int ii = 0; ii < 3; ii++)
+                    if (speed[ii] != 0) countSpeed = speedOfPlayer1;
+            System.out.println("Скорость игрока = " + countSpeed );
+            while (speedOfPlayer2 < 0)
+                for (int ii = 0; ii < 3; ii++)
+                    if (speed[ii] != 0) countSpeed2 = speedOfPlayer2;
+            System.out.println("Скорость игрока = " + countSpeed2 );
+            while (speedOfPlayer3 < 0)
+                for (int ii = 0; ii < 3; ii++)
+                    if (speed[ii] != 0) countSpeed3 = speedOfPlayer3;
+            System.out.println("Скорость игрока = " + countSpeed3 );
         }
         // если не красный (значит горит зелёный)
         else {
@@ -42,10 +48,19 @@ public class Main {
                 if (players[i] == 0) count++;
             }
             System.out.println("Горит зелёный, выбывает " + count + " игроков");
+            while (speedOfPlayer1 < 0)
+                for (int ii = 0; ii < 3; ii++)
+                    if (speed[ii] != 0) countSpeed = speedOfPlayer1;
+            System.out.println("Скорость игрока = " + countSpeed );
+            while (speedOfPlayer2 < 0)
+                for (int ii = 0; ii < 3; ii++)
+                    if (speed[ii] != 0) countSpeed2 = speedOfPlayer2;
+            System.out.println("Скорость игрока = " + countSpeed2 );
+            while (speedOfPlayer3 < 0)
+                for (int ii = 0; ii < 3; ii++)
+                    if (speed[ii] != 0) countSpeed3 = speedOfPlayer3;
+            System.out.println("Скорость игрока = " + countSpeed3 );
         }
 
     }
 }
-
-
-
