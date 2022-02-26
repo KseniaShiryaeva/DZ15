e88982357553cf4be28a35e8a5688c0371e23246
KseniaShiryaeva <100152965+KseniaShiryaeva@users.noreply.github.com>
2022-02-26 22:20:03 +0300

    Test

---
+++ src/speedTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+
+
+public class speedTest {
+    @Test
+
+public void   checkingTheSpeedArray () {
+
+        int speedOfPlayer1 = 0;
+        int speedOfPlayer2 = 6;
+        int speedOfPlayer3 = 7;
+
+        int players[] = {speedOfPlayer1, speedOfPlayer2, speedOfPlayer3};
+
+        int [] actual = {2};
+        int [] expected = {2};
+
+    }
+}
